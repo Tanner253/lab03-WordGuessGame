beed90d79be55668c155036ac57ad259eaed8f54
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-21 08:43:58 -0700

    commmits changes done on bus

--- lab03-WordGuessGame/lab03-WordGuessGame/Program.cs
+++ lab03-WordGuessGame/lab03-WordGuessGame/Program.cs
@@ -1,27 +1,55 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace lab03_WordGuessGame
 {
     public class Program
     {
         public static string path = ("../../../savedWords.txt");
+        public static bool gameRunning = true;
+
         public static void Main(string[] args)
         {
+            StartSequence();
+        }
+        public static void StartSequence()
+        {
+            do
+            {
+                string addedWord;
+                string[] startingWords = { "Biscuit", "Alpha", "Tarantula", "Rockstar" };
+                Console.WriteLine("Welcome to the Word Guess Game!");
+                WriteToFileMethod(startingWords);
+                Console.WriteLine("1. Play game");
+                Console.WriteLine("2. Admin");
+                Console.WriteLine("3. Exit Program");
+                int decision = Convert.ToInt32(Console.ReadLine());
+                switch (decision)
+                {
+                    case 1:
 
-            string[] startingWords = { "Biscuit", "Alpha", "Tarantula", "Rockstar"};
-            Console.WriteLine("Welcome to the Word Guess Game!");
-            WriteToFileMethod(startingWords);
+
+                        break;
+                }
+
+                Console.WriteLine("add a word");
+                addedWord = Console.ReadLine();
+                UpdateWordBank(addedWord);
+                ReadFile();
+            }
+            while (gameRunning);
+
         }
         public static void WriteToFileMethod(string[] startingWords)
         {
             using (StreamWriter sw = new StreamWriter(path))
             {
-                foreach  (string value in startingWords)
+                foreach (string value in startingWords)
                 {
                     sw.WriteLine(value);
                 }
-                
+
             }
         }
         public static void ReadFile()
@@ -31,9 +59,28 @@ namespace lab03_WordGuessGame
                 string[] textFileWords = File.ReadAllLines(path);
                 for (int i = 0; i < textFileWords.Length; i++)
                 {
-                    Console.WriteLine($"These are your options: {textFileWords[i]}");
+                    Console.WriteLine(textFileWords[i]);
                 }
             }
         }
+        public static void UpdateWordBank(string addedWord)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(addedWord);
+
+            }
+
+
+        }
+        public static void GuessCheck(char guessedChar)
+        {
+
+        }
+        public static string RandomWordChooser()
+        {
+
+        }
+
     }
 }
