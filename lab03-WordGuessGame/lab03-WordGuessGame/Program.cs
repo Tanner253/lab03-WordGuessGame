cd0f665dd42f5469001072067aeb56c8656c5090
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-20 22:10:32 -0700

    adds read and write methods

--- lab03-WordGuessGame/lab03-WordGuessGame/Program.cs
+++ lab03-WordGuessGame/lab03-WordGuessGame/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
 
 namespace lab03_WordGuessGame
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static string path = ("../../../savedWords.txt");
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string[] startingWords = { "Biscuit", "Alpha", "Tarantula", "Rockstar"};
+            Console.WriteLine("Welcome to the Word Guess Game!");
+            WriteToFileMethod(startingWords);
+        }
+        public static void WriteToFileMethod(string[] startingWords)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach  (string value in startingWords)
+                {
+                    sw.WriteLine(value);
+                }
+                
+            }
+        }
+        public static void ReadFile()
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string[] textFileWords = File.ReadAllLines(path);
+                for (int i = 0; i < textFileWords.Length; i++)
+                {
+                    Console.WriteLine($"These are your options: {textFileWords[i]}");
+                }
+            }
         }
     }
 }
