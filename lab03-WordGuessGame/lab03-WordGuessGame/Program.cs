f5e6381465530ad9bc743c2e88c7ea27c7092508
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-22 10:20:03 -0700

    adds 3 methos dealing with interface

--- lab03-WordGuessGame/lab03-WordGuessGame/Program.cs
+++ lab03-WordGuessGame/lab03-WordGuessGame/Program.cs
@@ -6,6 +6,7 @@ namespace lab03_WordGuessGame
 {
     public class Program
     {
+        
         public static string path = ("../../../savedWords.txt");
         public static bool gameRunning = true;
 
@@ -15,12 +16,13 @@ namespace lab03_WordGuessGame
         }
         public static void StartSequence()
         {
+                string[] startingWords = { "Biscuit", "Alpha", "Tarantula", "Rockstar", "Cookies", "Doggo" };
+                WriteToFileMethod(startingWords);
             do
             {
-                string addedWord;
-                string[] startingWords = { "Biscuit", "Alpha", "Tarantula", "Rockstar" };
+                //string addedWord;
+                
                 Console.WriteLine("Welcome to the Word Guess Game!");
-                WriteToFileMethod(startingWords);
                 Console.WriteLine("1. Play game");
                 Console.WriteLine("2. Admin");
                 Console.WriteLine("3. Exit Program");
@@ -28,15 +30,24 @@ namespace lab03_WordGuessGame
                 switch (decision)
                 {
                     case 1:
+                        ReadFile();
 
+                        Console.WriteLine(RandomWordChooser(startingWords));
 
+                        break;
+                    case 2:
+                        AdminMenu();
+                        break;
+
+                    case 3:
+                        gameRunning = false;
                         break;
                 }
 
-                Console.WriteLine("add a word");
+               /* Console.WriteLine("add a word");
                 addedWord = Console.ReadLine();
                 UpdateWordBank(addedWord);
-                ReadFile();
+                ReadFile(); */
             }
             while (gameRunning);
 
@@ -57,11 +68,11 @@ namespace lab03_WordGuessGame
             using (StreamReader sr = new StreamReader(path))
             {
                 string[] textFileWords = File.ReadAllLines(path);
-                for (int i = 0; i < textFileWords.Length; i++)
-                {
-                    Console.WriteLine(textFileWords[i]);
-                }
+               
+                
             }
+            
+          
         }
         public static void UpdateWordBank(string addedWord)
         {
@@ -77,10 +88,55 @@ namespace lab03_WordGuessGame
         {
 
         }
-        public static string RandomWordChooser()
+        public static string RandomWordChooser(string[] array)
         {
-
+            Random random = new Random();
+            int randomNumber = random.Next(0, array.Length);
+            string displayWord = array[randomNumber];
+            return displayWord;
         }
+        public static void AdminMenu()
+        {
+            Console.WriteLine("1. View Current Word Bank");
+            Console.WriteLine("2. Add a word to Word Bank");
+            Console.WriteLine("3. Remove a word from Word Bank");
+            Console.WriteLine("4. Main Menu");
+            int decision = Convert.ToInt32(Console.ReadLine());
+            
+           
+
+                switch (decision)
+                {
+                    case 1:
+                        Console.WriteLine("These are the current words in the Word Bank: ");
+                        
+                        
+                        break;
+
+                    case 2:
+                    Console.WriteLine("You've selected to ADD a word to the Word Bank");
+                    Console.WriteLine("");
+                    Console.WriteLine("Please type the word you would like to ADD to the word bank");
+                    UpdateWordBank(Console.ReadLine());
+
+                        break;
+
+                    case 3:
+                    Console.WriteLine("You've selected to REMOVE a word to the Word Bank");
+                    Console.WriteLine("");
+                    Console.WriteLine("Please type the word you would like to REMOVE from the word bank");
+                    ReadFile();
+                    
+                    break;
+
+                    case 4:
+                    StartSequence();
+                        break;
+                }
+            
+            
+        }
+        
 
     }
 }
